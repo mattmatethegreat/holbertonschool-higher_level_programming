594031974631291db32b979c804ab9811e72b587
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-11 13:18:30 -0700

    fixed typos

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def print_list_integers(my_list=[]);
-for num in my_list:
-    print(num)
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print(num)
 
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
