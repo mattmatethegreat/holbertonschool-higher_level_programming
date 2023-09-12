e7349eeedeb39958885b4e355c51d2a531b3d63e
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-11 18:25:39 -0700

    adapted file to work with main.

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-def print_list_integer(*args):
-    my_list = list(args)
-
+def print_list_integer(my_list):
     for num in my_list:
         print(num)
-
-print_list_integer(1, 2, 3, 4, 5)
