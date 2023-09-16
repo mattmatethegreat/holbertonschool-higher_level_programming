d4c56a83a49fd87deb1759d304514de9de4a6b45
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-16 13:51:26 -0700

    fixed pycodestyle

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -4,5 +4,5 @@ def common_elements(set_1, set_2):
     common_set = set()
     for element in set_1:
         if element in set_2:
-         common_set.add(element)
+            common_set.add(element)
     return common_set
