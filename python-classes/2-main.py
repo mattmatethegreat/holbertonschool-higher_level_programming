03e4cce1a74e8228d21f82d216de2959e8278f2c
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-18 00:42:45 -0700

    fixed 2?

--- python-classes/2-main.py
+++ python-classes/2-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 Square = __import__('2-square').Square
 
 my_square_1 = Square(3)
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""" Thius module defines the square class """
-
 
 class Square:
-
-    """
-    This class represents a square.
+    """A class representing a square.
 
     Attributes:
-    __size (int): The size of the square.
+        size (int): The size of the square.
     """
 
+    def __init__(self, size=0):
+        """Initialize the Square instance.
 
-def __init__(self, size=0):
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
 
-    """
-    Initializes a new instance of the Square class.
-
-    Args:
-    size (int): The size of the square. Defaults to 0.
-
-    Raises:
-    TypeError: If size is not an integer.
-    ValueError: If size is less than 0.
-    """
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
