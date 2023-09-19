6dd02b4a4a63e6386dda38c5e601ca82ed98652d
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-19 15:53:05 -0700

    trying to complete, first half rushed, runnign checks.

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10, 10)
+
+if number > 0:
+    print(f"The number is positive: {number}")
+elif number == 0:
+    print(f"The number is zero: {number}")
+else:
+    print(f"The number is negative: {number}")
---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/user/bin/python3
+
+for num in range(99):
+    print("{0} {1}".format(num, hex(num)[2:]))
