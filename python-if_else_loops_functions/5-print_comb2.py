33239f15f3bdac86d6f962e41386b6ee4ac49a46
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-19 18:40:00 -0700

    changed format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 for i in range(100):
-    print(f"{i:02d}", end="")
+    print("{:02d}".format(i), end="")
     if i != 99:
         print(", ", end="")
     else:
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end="")
+        print("{}{}".format(i, j), end="")
         if i != 8 or j != 9:
             print(", ", end="")
         else:
