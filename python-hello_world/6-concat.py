62de2c920c41147d798b22c1a465022d672e5f89
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-19 17:58:08 -0700

    finished

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
-str1 = "Welcome to"
-str2 = " Holberton School!"
-print(str1 + str2)
+word = "Python"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"{word_first_3} {word_last_2} {middle_word}")
