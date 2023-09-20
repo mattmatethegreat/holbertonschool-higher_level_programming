ad2b33b9b970a7e0abbe9e39e5c9eb2bc8ede9bb
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-20 02:57:24 -0700

    eh

--- python-exceptions/4-main.py
+++ python-exceptions/4-main.py
@@ -12,11 +12,3 @@ my_l_1 = [10, 8, 4, 4]
 my_l_2 = [2, 0, "H", 2, 7]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
 print(result)
-
-guillaume@ubuntu:~/$ ./4-main.py
-[5.0, 2.0, 1.0]
---
-division by 0
-wrong type
-out of range
-[5.0, 0, 0, 2.0, 0]
