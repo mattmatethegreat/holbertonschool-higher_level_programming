850dedd90972af1df82a779f85240458d70a5b03
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-19 18:34:15 -0700

    fixed swp file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-def print_numbers():
-    print("01", end="")
-    for num in range(10, 100):
-        if num % 10 != num // 10:
-            print(", {:02d}".format(num), end="")
-    print()
-
-print_numbers()
-
+for i in range(100):
+    print(f"{i:02d}", end="")
+    if i != 99:
+        print(", ", end="")
+    else:
+        print()
