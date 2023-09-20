ad595059eeb5734a5ff0fa2ae72c9d6e116851ed
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-20 16:29:16 -0700

    trying to finish loose ends

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 str1 = "Welcome to"
 str2 = "Holberton School!"
-print("{} {}".format(str1, str2))\
+print("{} {}".format(str1, str2))
--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-print(str1, end=" ")
-print(str2)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
