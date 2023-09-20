7d24c2df18008ce8a7a7f12e0ff2cabee8c595a2
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-20 02:02:59 -0700

    ?

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,8 @@
 def print_last_digit(number):
     last_digit_str = str(number)[-1]
     last_digit = int(last_digit_str)
-    print(last_digit)
+    return last_digit
 
-print_last_digit(8044)
+# Test the function
+result = print_last_digit(8044)
+print(result)
