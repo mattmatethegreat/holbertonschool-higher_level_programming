d2a2adaf8d67ff18298be30ebcd1c6ae488c3b3f
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-20 01:47:43 -0700

    fixes

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{i} = {format(i, 'd')} = {format(i, 'x')}")
+    print("{} = {} = {}".format(i, format(i, 'd'), format(i, 'x')))
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python3
 def uppercase(string):
+    uppercase_string = ""
     for char in string:
-        ascii_value = ord(char)
-        if 97 <= ascii_value <= 122:
-            uppercase_char = chr(ascii_value - 32)
-        else:
-            uppercase_char = char
-        print(uppercase_char, end="")
-    print()
+        uppercase_char = char.upper()
+        uppercase_string += uppercase_char
+    print(uppercase_string)
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
