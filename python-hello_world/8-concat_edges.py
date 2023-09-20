55ad5c6ea8bc9e786bbbebc1e46e45befff28bd6
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-19 18:08:34 -0700

    l

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-word = "Python"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print(f"{word_first_3} {word_last_2} {middle_word}")
+str1 = "Welcome to"
+str2 = "Holberton School!"
+print(str1, end=" ")
+print(str2)
