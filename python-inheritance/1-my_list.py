03af80daca783300aaac6f6aac124ec42ab1a2c0
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-24 20:53:43 -0700

    first two on second project

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+class MyList(list):
+    """ Custom list class that inherits from the built-in list class."""
+
+    def print_sorted(self):
+        """
+        Print in ascending order.
+
+        Returns:
+            None
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is my readme on inherritance with pyython
