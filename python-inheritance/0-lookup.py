42f330fafb6e6b78015bcc8d86546e6faa091466
mattmatethegreat <matthgraham1996@gmail.com>
2023-09-24 21:36:03 -0700

    working on 1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,10 +3,10 @@
 def lookup(obj):
     """
     Returns a list of attributes and methods of the given object.
-    
+
     Args:
         obj: The object to look up.
-    
+
     Returns:
         A list of attribute and method names as strings.
     """
