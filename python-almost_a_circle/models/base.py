58a1e2cbfc7bf803c10540f7a85a4b3e7eb4966c
mattmatethegreat <matthgraham1996@gmail.com>
2023-10-01 21:28:03 -0700

    basE

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+""" base """
 class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Initializes base 
+        Args: id: identity of base."""
+
+
         if id is not None:
             self.id = id
         else:
--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -135,5 +135,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the print and string of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
